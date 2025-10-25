cf4b75138656fb4a1f510bfc7b452bf6f68ad950
sultanjke <s.mecheyev@outlook.com>
2025-10-25 05:42:43 +0500

    Navbar and sidebar inital commits

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,8 +1,54 @@
+"use client"
+
+import { Bell, Link, Link2, LinkIcon, Menu, Settings, Sun } from 'lucide-react'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+  
+    <div className="flex justify-between items-center w-full mb-7">
+      {/* LEFT SIDE */}
+      <div className="flex justify-between items-center gap-5">
+        <button className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={() => {}}>
+          <Menu className="w-4 h-4" /> 
+        </button>
+      
+        <div className="relative">
+          <input 
+            type="search" placeholder="Start type to search groups & products" className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"  
+          /> 
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none" >
+            <Bell className="text-gray-500" size={20} />
+          </div>
+        </div>
+      </div>
+
+      {/* RIGHT SIDE */}
+
+      <div className="flex justify-between items-center gap-5">
+        <div className="hidden md:flex justify-between items-center gap-5">
+          <div>
+            <button onClick={() => {}}>
+              <Sun className="cursor-pointer text-gray-500" size={24}/>
+            </button>
+          </div>
+          <div className="relative">
+            <Bell className="cursor-pointer text-gray-500" size={24}/>
+            <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+              3
+            </span>
+          </div>
+          <hr className="w-0 h-7 border-solid border-l border-gray-300 mx-3" />
+          <div className="flex items-center gap-3 cursor-pointer">
+            <div className="w-9 h-9">image</div>
+          </div><span className="font-semibold">John Doe</span>
+        </div>
+      </div>
+      <a href="/settings" aria-label="Settings">
+        <Settings className="cursor-pointer text-gray-500" size={24} />
+      </a>
+    </div>
+
   )
 }
 

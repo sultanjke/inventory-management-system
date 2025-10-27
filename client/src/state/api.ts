dd664dc55ed5cd5c6069b6455fa6b450c7481050
sultanjke <s.mecheyev@outlook.com>
2025-10-28 03:34:54 +0500

    Initial clean commit

---
+++ client/src/state/api.ts
@@ -0,0 +1,66 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export interface Product {
+    productId: string;
+    name: string;
+    price: number;
+    rating?: number;
+    stockQuantity: number;
+}
+
+export interface SalesSummary {
+    salesSummaryId: string;
+    totalValue: number;
+    changePercentage?: number;
+    date: string;
+
+}
+
+export interface PurchaseSummary {
+    purchaseSummaryId: string;
+    totalPurchased: number;
+    changePercentage?: number;
+    date: string;
+
+}
+
+export interface ExpenseSummary {
+
+    expenseSummaryId: string;
+    totalExpenses: number;
+    date: string;
+
+}
+
+export interface ExpenseByCategorySummary {
+
+    expenseByCategorySummaryId: string;
+    category: string;
+    amount: string;
+    date: string;
+
+}
+
+export interface DashboardMetrics {
+
+    popularProducts: Product[];
+    salesSummary: SalesSummary[];
+    purchaseSummary: PurchaseSummary[];
+    expenseSummary: ExpenseSummary[];
+    expenseByCategorySummary: ExpenseByCategorySummary[];
+
+}
+
+export const api = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api" }),
+    reducerPath: "api",
+    tagTypes: ["DashboardMetrics"],
+    endpoints: (build) => ({
+        getDashboardMetrics: build.query<DashboardMetrics, void>({  
+            query: () => "/dashboard",
+            providesTags: ["DashboardMetrics"]
+        }),
+    }),
+});
+
+export const {} = api;
---
+++ server/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+/* ROUTE IMPORTS */
+import dashboardRoutes from "./routes/dashboardRoutes";
+
+/* CONFIGURATIOSN */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES  */
+app.use("/dashboard", dashboardRoutes); // http://localhost:3001/dashboard
+
+/* SERVER */
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); 
